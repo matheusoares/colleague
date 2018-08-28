ead7667f10d092dbf3fe8c1af852fce12e9d43b5
Matheus Soares de Sousa <matheus_desousa@live.com>
2018-08-28 14:51:23 -0300

    mobile fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).on("scroll", function() {
 
-	if($(document).scrollTop()>400) {
+	if($(document).scrollTop()>200) {
 		$("header").removeClass("large").addClass("small");
 	} else {
 		$("header").removeClass("small").addClass("large");
