ee3004ab9738cb9aec567ac3ae8c985cc87d6091
Matheus Soares de Sousa <matheus_desousa@live.com>
2018-08-30 00:18:08 -0300

    mobile menu ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,21 @@ $(document).on("scroll", function() {
 	}
 	
 });
+
+var btn = document.getElementById("btnTrigger");
+var menu = document.getElementById("menuHidded");
+
+btn.addEventListener('click', function(){
+	menu.classList.toggle('menu-hidded');
+
+	if (!menu.classList.contains('menu-hidded')) {
+		btn.classList.add('open');
+		$('body').css("overflow", "hidden");
+	}else{
+		btn.classList.remove('open');
+		$('body').css("overflow", "initial");
+	}
+
+});
+
+
