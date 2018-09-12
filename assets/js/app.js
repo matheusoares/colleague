4be9b7a8e75bb3ce6979d0bd26cee449744600f3
Matheus Soares de Sousa <matheus_desousa@live.com>
2018-09-12 14:58:21 -0300

    new layout updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,4 +24,28 @@ btn.addEventListener('click', function(){
 
 });
 
+function initMap() {
+	var beDifferent = { lat: -31.7541639, lng: -52.3174417 };
+	var map = new google.maps.Map(
+		document.getElementById('map'), { zoom: 17, center: beDifferent });
+
+	var contentString = '<div id="content" style="width: 256px;">' +
+		'<div id="siteNotice">' +
+		'</div>' +
+		'<img id="firstHeading" class="firstHeading" src="assets/img/logo.svg" alt="logomarca da beDifferent" >' +
+		'<div id="bodyContent">' +
+		'<p>Pelotas Parque Tecnológico, Av. Domingos de Almeida, 1785 - Areal, Pelotas - RS. </p>' +
+		'</div>' +
+		'</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+	
+	var marker = new google.maps.Marker({ position: beDifferent, map: map,  title: 'be Different' });
+	$( document ).ready(function() {
+		infowindow.open(map, marker);
+	});
+}
+
 
